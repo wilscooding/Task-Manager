6c1702e7b087e125521e7f33c1f35843d4451106
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-03-06 14:58:35 -0500

    Feature 4 complete

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "owner",
+    )
+
+
+admin.site.register(Project, ProjectAdmin)
