9489aaa5dac050e3414ca80bd8f18b331931e6ca
wilkin ruiz <wilkin.ruizvalerio@gmail.com>
2023-03-06 15:39:00 -0500

    Feature 6 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def redirect_home(request):
+    return redirect("list_projects")
+
 
 urlpatterns = [
+    path('', redirect_home, name='home'),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
 ]
